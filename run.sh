b5b01df2c98beb65088af947f068675565b4941f
Todd Brandt <todd.e.brandt@linux.intel.com>
2018-06-02 20:15:44 -0700

    notes on how to move forward

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-./musualizer -v -d alsa_output.pci-0000_00_1b.0.analog-stereo.monitor --rate=192000 -w hamming -t "8:0:-1"
+./musualizer -v -d alsa_output.pci-0000_00_1b.0.analog-stereo.monitor --rate=192000 -w hamming -t "4:0:-1"
